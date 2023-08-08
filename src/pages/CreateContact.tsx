d2ff2f6df0495728dbce28723b1a30da5852dbb2
shoqqan <tataev.shokan@gmail.com>
2023-08-09 03:00:47 +0600

    ui fix

--- src/pages/CreateContact.tsx
+++ src/pages/CreateContact.tsx
@@ -7,10 +7,10 @@ export const CreateContact = () => {
     return (
         <div className={'w-full h-screen flex flex-col items-center py-12 lg:py-0'}>
             <div
-                className={'w-[800px] bg-zinc-700 flex flex-col items-center gap-y-12 py-12 cursor-pointer rounded-xl shadow-2xl lg:w-full rounded-none h-screen py-0'}>
-                <div className={'w-full px-12 lg:mt-5'}>
+                className={'w-[800px] bg-zinc-700 flex flex-col items-center gap-y-12 py-12 cursor-pointer rounded-xl shadow-2xl lg:w-full lg:rounded-none lg:h-screen lg:py-0'}>
+                <div className={'w-full px-12 lg: mt-5'}>
                     <div
-                        className={'w-[40px] h-[40px] flex justify-center items-center bg-white rounded-full hover:scale-110 ease-in-out duration-300 lg:w-[50px] h-[50px]'}
+                        className={'w-[40px] h-[40px] flex justify-center items-center bg-white rounded-full hover:scale-110 ease-in-out duration-300 lg:w-[50px] lg:h-[50px]'}
                         onClick={() => {
                             navigate('/')
                         }}
